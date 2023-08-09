02ef0d2ef58bfd57d1f763b2640aedcad463ab4c
McDonald Sebastine <sabastinemc@gmail.com>
2023-08-09 14:29:09 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Payblauq web2/web3 payment gateway
         </p>
       </div>
       <p className="read-the-docs">
